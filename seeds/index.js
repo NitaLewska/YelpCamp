9e559e774dc317829952d2c2f281d2a0a078a772
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-01-08 14:07:43 +0300

    feat: add basic styling, format most files
    S41: YelpCamp: Adding Basic Styles
    
    basic styling made with bootstrap 5

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
-const Campground = require('../models/campground') 
+const { places, descriptors } = require('./seedHelpers')
+const Campground = require('../models/campground')
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -20,13 +20,16 @@ const sample = arr => arr[Math.floor(Math.random() * arr.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for(let i=0; i<50; i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}` 
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            price
         })
-
         await camp.save()
     }
 }
